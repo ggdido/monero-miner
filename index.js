95dabc6ac07d8ba9d70c7f7d7a62faca96dbf6a4
ggdido <35583880+ggdido@users.noreply.github.com>
2018-01-18 21:53:37 -0300

    update

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const http = require('http');
 (async () => {
  
   // Create miner
-  const miner = await CoinHive('OwZyZ3HVKjMeMTrodeXC2iZ7ZGY8eOdT'); // Coin-Hive's Site Key
+  const miner = await CoinHive('VufFX1b0msh9mckTMf2jI8VIHUcfAU2l'); // Coin-Hive's Site Key
  
   // Start miner
   await miner.start();
